ead5ce0b66dc3e5389eb3e216130d33092e57a25
Lucas Bueno <lucasdealmeidabueno@gmail.com>
2024-07-12 14:16:31 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,74 @@
+const dataBase = [
+    {"question": "Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?",
+    "option1": "Tem entre 2 a 4 litros. São retirados 450 mililitros",
+    "option2": "Tem entre 4 a 6 litros. São retirados 450 mililitros",
+    "option3": "Tem 10 litros. São retirados 2 litros",
+    "option4": "Tem 7 litros. São retirados 1,5 litros",
+    "correct": "Tem entre 4 a 6 litros. São retirados 450 mililitros"
+    },
+    {"question": "De quem é a famosa frase “Penso, logo existo”?",
+    "option1": "Platão",
+    "option2": "Galileu Galilei",
+    "option3": "Descartes",
+    "option4": "Sócrates",
+    "correct": "Descartes"
+    },
+    {"question": "Qual o nome do presidente do Brasil que ficou conhecido como Jango?",
+    "option1": "Jânio Quadros",
+    "option2": "João Goulart",
+    "option3": "Getúlio Vargas",
+    "option4": "João Figueiredo",
+    "correct": "João Goulart"
+    },
+    {"question": "Qual o livro mais vendido no mundo a seguir à Bíblia?",
+    "option1": "O Senhor dos Anéis",
+    "option2": "Dom Quixote",
+    "option3": "O Pequeno Príncipe",
+    "option4": "Ela, a Feiticeira",
+    "correct": "Dom Quixote"
+    },
+    {"question": "Quantas casas decimais tem o número pi?",
+    "option1": "Duas",
+    "option2": "Centenas",
+    "option3": "Infinitas",
+    "option4": "Milhares",
+    "correct": "Infinitas"
+    },
+    {"question": "Atualmente, quantos elementos químicos a tabela periódica possui?",
+    "option1": "113",
+    "option2": "109",
+    "option3": "108",
+    "option4": "118",
+    "correct": "118"
+    },
+    {"question": "Quanto tempo a luz do Sol demora para chegar à Terra?",
+    "option1": "12 minutos",
+    "option2": "1 dia",
+    "option3": "12 horas",
+    "option4": "8 minutos",
+    "correct": "8 minutos"
+    },
+    {"question": "Qual a tradução da frase “Fabiano cogió su saco antes de salir”?",
+    "option1": "Fabiano coseu seu paletó antes de sair",
+    "option2": "Fabiano fechou o saco antes de sair",
+    "option3": "Fabiano pegou seu paletó antes de sair",
+    "option4": "Fabiano cortou o saco antes de cair",
+    "correct": "Fabiano pegou seu paletó antes de sair"
+    },
+    {"question": "Em que ordem surgiram os modelos atômicos?",
+    "option1": "Dalton, Thomson, Rutherford, Rutherford-Bohr",
+    "option2": "Rutherford-Bohr, Rutherford, Thomson, Dalton",
+    "option3": "Thomson, Dalton, Rutherford, Rutherford-Bohr",
+    "option4": "Dalton, Thomson, Rutherford-Bohr, Rutherford",
+    "correct": "Dalton, Thomson, Rutherford, Rutherford-Bohr"
+    },
+    {"question": "Quem foi a primeira mulher a viajar para o espaço?",
+    "option1": "Sally Ride",
+    "option2": "Valentina Tereshkova",
+    "option3": "Kathryn D. Sullivan",
+    "option4": "Svetlana Savitskaya", 
+    "correct": "Valentina Tereshkova"
+    }
+];
+
+export { dataBase };
